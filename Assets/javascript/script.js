47ff243c15d69bd803cdceb88cb732e868cab2ed
DaymensCode <Daymenscode@gmail.com>
2023-11-04 14:36:08 -0400

    added more js pseudo-code

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -16,10 +16,9 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // Need clickable button that will present password criteria prompt
-// 1)Need button
-// q)maybe set this button next to/replace "Generate Password" with text saying "set password criteria"?
-// b)make the current "Generate Password" button grayed out/unclickable untill we've set the criteria with the afore-mentioned button
-// c)give a hover/active message over "Generate Password" button that tells the user something like "no criteria set"
+// 1)Use button that's already present
+
+
 //2) Need way to present a prompt as a result of clicking the "set password criteria" button
 
 
@@ -33,22 +32,25 @@ generateBtn.addEventListener("click", writePassword);
 
 // 2) need a way to present this list list of creteria (preferrable in toggle format)
 
-// 3) need a way to take in the user's inputted creteria prefferences, and apply these settings to the-
-// algorithm that will be used to populate the random password
+
 
 
 
 //Pw length can only be 8 ~ 128 chars
+// 1) need a way to control the min/max length of characters generated when user generated the password
+
 
 
 
 // Toggle to confirm which types of characters can be included: upper/lowercase, numeric, special...
-
+// 1) need a way to take in the user's inputted creteria prefferences, and apply these settings to the-
+// algorithm that will be used to populate the random password
 
 
 
 // Minimum of one character type needs to be selected
-
+// 1)some type of code to prevent the ability to generate password if no-character type is set
+// 2) a prompt message saying they must select at least one type of character before generation can start
 
 
 
@@ -56,8 +58,13 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-//After every prompts are answered, pw is generated that matches selected criteria
+//After every prompt is answered, pw is generated that matches selected criteria
+// 1) loop that takes in all of the previous functions or whatever type of code that handled criteria input-
+// and uses it to generate a random pw based off input
+
 
 
 
 //When this pw is written, its either displayed in the alert or written to the page
+// 1) code that will interact with html/css to relay the generated password into the box section of html-
+// containing the "Your Secure Password" text

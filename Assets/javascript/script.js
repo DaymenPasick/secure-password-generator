ea1af7f8779aa32923025372949ddbc5395947c8
DaymensCode <Daymenscode@gmail.com>
2023-11-04 14:52:03 -0400

    added some reference code in js and basic variables

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -26,9 +26,18 @@ generateBtn.addEventListener("click", writePassword);
 // Need selectable list of criteria to include in pw
 // 1)need to create global variables for each list type
 // a)for lowercase alph
+var lowerCase = "";
 // b)for uppercase alph
+var upperCase = "";
 // c)for numeric
+var numeric = 0 ;
 // d)for symbols/random chars
+var symbols = "";
+
+
+
+
+
 
 // 2) need a way to present this list list of creteria (preferrable in toggle format)
 
@@ -50,8 +59,16 @@ generateBtn.addEventListener("click", writePassword);
 
 // Minimum of one character type needs to be selected
 // 1)some type of code to prevent the ability to generate password if no-character type is set
-// 2) a prompt message saying they must select at least one type of character before generation can start
+if (noCharSelection = true) {
+  generatorCommence = false; 
+}
 
+// 2) a prompt message saying they must select at least one type of character before generation can start
+if (generatorCommence = false) {
+  // this console.log should be replaced with an actual prompt screen message
+  console.log("at least one criteria must be selected");
+  // need code to return user to criteria options/toggle interface
+}
 
 
 // When I answer each prompt, input should be validated

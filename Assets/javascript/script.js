d3e7b9373123bbfc4d57d7cf744491d7043219ec
DaymensCode <Daymenscode@gmail.com>
2023-11-04 11:29:29 -0400

    added planning comments in js file

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -12,3 +12,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Need clickable button that will present password criteria prompt
+
+
+
+// Need selectable list of criteria to include in pw
+
+
+
+//Pw length can only be 8 ~ 128 chars
+
+
+
+// Toggle to confirm which types of characters can be included: upper/lowercase, numeric, special...
+
+
+
+
+// Minimum of one character type needs to be selected
+
+
+
+
+// When each prompt has been answered, input should be validated

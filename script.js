61f2baa41b9c0fa112b1d6fe1b0dd672ac160093
DaymensCode <Daymenscode@gmail.com>
2023-11-04 22:52:51 -0400

    added to userCriteriaPrompt function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 
 // this is setting a variable based off of the item specified in the querySelector. (#generate ID'd button in the HTML)
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 // function writePassword() {
@@ -20,45 +20,25 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-// Need clickable button that will present password criteria prompt
-
+// Need clickable button that will present password criteria prompt --------------------------------------------------------------------------
 // This will populate a generic prompt and record answer in console log for testing
- function setCriteriaPrompt() {
-  var criteriaAlert = prompt("please select desired criteria");
+ function userCriteriaPrompt() {
+  var inputCriteria = prompt("please select desired criteria");
   alert ("thank you");
-  console.log(criteriaAlert)
+  if (inputCriteria === 'y' || inputCriteria === 'Y' || inputCriteria === 'n' || inputCriteria === 'N') {
+    console.log ("woop woop");
+  } else {
+    console.log("please try again")
+  }
+}
 // need to make this a loop that will run through all of the criteria prompts and record user-response  
- }
+
+console.log("log tester");
 
 
+// Need selectable list of criteria to include in pw --------------------------------------------------------------------------
+// 1)Global variables for each list type
 
-// Need selectable list of criteria to include in pw
-// 1)need to create global variables for each list type
-// a)prompt for lowercase alph
-function lowerCasePrompt() {
-  prompt("would you like to include lower case letters?")
-}
-// b)prompt for uppercase alph
-function upperCasePrompt() {
-  prompt("would you like to include upper case letters?")
-}
-// c)prompt for numeric
-function numericPrompt() {
-  prompt("would you like to include numbers?")
-}
-// d)prompt for symbols/random chars
-function symbolsPrompt() {
-  prompt("would you like to include symbols? (I.G $,#,@)")
-}
-
-
-
-
-
-
-
-// 2) need a way to present list list of creteria (preferrable in toggle format)
-// Will have to use multiple prompts
 // a)for lowercase alph
 var lowerCase = "";
 // b)uppercase alph
@@ -68,16 +48,30 @@ var numeric = 0 ;
 // d)for symbols/random chars
 var symbols = "";
 
+// 2) need a way to present list list of creteria (preferrable in toggle format)
+//Functions for each criteria option prompt
+function lowerCasePrompt() {
+    prompt("would you like to include lower case letters?")
+  }
+  
+  function upperCasePrompt() {
+    prompt("would you like to include upper case letters?")
+  }
+  
+  function numericPrompt() {
+    prompt("would you like to include numbers?")
+  }
+  
+  function symbolsPrompt() {
+    prompt("would you like to include symbols? (I.G $,#,@)")
+  }
 
 
 
 
 
-//Pw length can only be 8 ~ 128 chars
+//Pw length can only be 8 ~ 128 chars --------------------------------------------------------------------------
 // 1) need a way to control the min/max length of characters generated when user generated the password
-
-
-
 
 // Toggle to confirm which types of characters can be included: upper/lowercase, numeric, special...
 // 1) need a way to take in the user's inputted creteria prefferences, and apply these settings to the-
@@ -95,8 +89,6 @@ var symbolToggle = true
 //   var upperCaseToggle = false;
 // }
 
-
-
 // Minimum of one character type needs to be selected
 // 1)some type of code to prevent the ability to generate password if no-character type is set
 if (noCharSelection = true) {
@@ -111,11 +103,15 @@ if (generatorCommence = false) {
 }
 
 
-// When I answer each prompt, input should be validated "please enter correct......"
+// When I answer each prompt, input should be validated "please enter correct......" --------------------------------------------------------------------------
+
+// if (inputCriteria === 'y' || inputCriteria === 'Y' inputCriteria === 'n' inputCriteria === 'N') {
+//     console.log ("woop woop");
+// }
 
 
 
-//After every prompt is answered, pw is generated that matches selected criteria
+//After every prompt is answered, pw is generated that matches selected criteria --------------------------------------------------------------------------
 // 1) loop that takes in all of the previous functions or whatever type of code that handled criteria input-
 // and uses it to generate a random pw based off input
  if (generatorCommence = true) {
@@ -126,6 +122,6 @@ if (generatorCommence = false) {
 
 
 
-//When this pw is written, its either displayed in the alert or written to the page
-// 1) code that will interact with html/css to relay the generated password into the box section of html-
-// containing the "Your Secure Password" text
+//When this pw is written, its either displayed in the alert or written to the page--------------------------------------------------------------------------
+
+// use the above given code in the starter code to facilitate this :)
